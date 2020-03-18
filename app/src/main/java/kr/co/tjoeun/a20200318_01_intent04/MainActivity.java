d90665004c3ad3a90d022fb6c52bbaa8be9709e8
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-18 09:48:21 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/MainActivity.java
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.a20200318_01_intent04;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200318_01_intent04.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
